50ce1802134fa23617392c175d25cb85f2f98027
Vasily Chinarev <vasa.chi@gmail.com>
2013-12-23 19:56:58 +0400

    Отправка разных типов сообщений

--- script.js
+++ script.js
@@ -5,27 +5,51 @@
  */
 
 var argv = require('optimist')
-  .usage('usage: $0 --uniqueid [string] --callerid [string] --local [string] --file [string] --message [string] --socket [URL]')
+  .usage('usage: $0 --action [incoming|call_picked|call_hangup] --callerid [string] --uniqueid [string] --local [string] --file [string] --message [string] --socket [URL]')
+  .alias('a', 'action')
   .alias('u', 'uniqueid')
   .alias('c', 'callerid')
   .alias('l', 'local')
   .alias('f', 'file')
   .alias('m', 'message')
   .alias('s', 'socket')
+  .check(function (argv) {
+    return /^(incoming|call_picked|call_hangup)$/.test(argv.action)
+  })
+  .check(function (argv) {
+    switch (argv.action) {
+      case 'incoming':
+        return (typeof argv.callerid == "string");
+        break;
+      case 'call_picked':
+        return (typeof argv.callerid == "string") &&
+               (typeof argv.uniqueid == "string") &&
+               (typeof argv.file == "string") &&
+               (typeof argv.local == "string");
+        break;
+      case 'call_hangup':
+        return (typeof argv.uniqueid == "string");
+        break;
+      default:
+        return false;
+    }
+  })
+  .string('a')
   .string('u')
   .string('c')
   .string('l')
   .string('f')
   .string('m')
   .string('s')
-  .demand(['u', 'c', 'l', 'f'])
+  .demand(['a'])
+  .describe('a', 'Action: incoming|call_picked|call_hangup')
   .describe('u', 'Unique ID')
   .describe('c', 'Caller ID')
   .describe('l', 'local')
   .describe('f', 'path to call recording')
   .describe('m', 'message')
   .describe('s', 'URL to websocket')
-  .default('s', 'http://127.0.0.1:9999/socket')
+  .default('s', 'http://127.0.0.1:9999/phone')
   .argv;
 
 var SockJs = require('sockjs-client');
@@ -42,14 +66,34 @@ client
       }
     });
 
-    client.write(JSON.stringify({
-      _uuid   : uuid.v1(),
-      uniqueid: argv.uniqueid,
-      callerid: argv.callerid,
-      local   : argv.local,
-      file    : argv.file,
-      message : argv.message
-    }));
+    switch (argv.action) {
+      case 'incoming':
+        client.write(JSON.stringify({
+          _uuid   : uuid.v1(),
+          type    : 'incoming',
+          callerid: argv.callerid,
+          message : argv.message
+        }));
+        break;
+      case 'call_picked':
+        client.write(JSON.stringify({
+          _uuid   : uuid.v1(),
+          type    : 'call_picked',
+          uniqueid: argv.uniqueid,
+          callerid: argv.callerid,
+          local   : argv.local,
+          file    : argv.file,
+          message : argv.message
+        }));
+        break;
+      case 'call_hangup':
+        client.write(JSON.stringify({
+          _uuid   : uuid.v1(),
+          type    : 'call_hangup',
+          uniqueid: argv.uniqueid
+        }));
+        break;
+    }
 
     setTimeout(function () {
       process.exit("timeout")
